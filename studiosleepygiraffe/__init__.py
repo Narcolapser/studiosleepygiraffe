62728e9f4e53b09eb40cda205d81d740b93f00ac
Toben Archer <sandslash@gmail.com>
2017-03-20 21:47:33 +0000

    Got templates working.

--- studiosleepygiraffe/__init__.py
+++ studiosleepygiraffe/__init__.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Studio Sleepy Giraffe. Website coming soon."
+	return render_template('index.html')
 
 @app.route("/version")
 def version():
-	return "0.0.6"
+	return "0.0.7"
 
 if __name__ == "__main__":
 	app.run()
