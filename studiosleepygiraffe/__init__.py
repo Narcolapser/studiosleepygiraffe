305cbc0aba7cba9eeab0a4671666b5d4a16bde20
Toben Archer <Toben.Archer@usd.edu>
2017-04-20 13:54:18 -0500

    Some basic layout coming in.
    I've got a long way to go.

--- studiosleepygiraffe/__init__.py
+++ studiosleepygiraffe/__init__.py
@@ -1,18 +1,54 @@
 from flask import Flask, render_template, send_file
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
 
 app = Flask(__name__)
+Bootstrap(app)
+
+nav = Nav()
+@nav.navigation()
+def mynavbar():
+        return Navbar('Stduio Sleepy Giraffe',
+                      View('Home', 'home'),
+                      View('Apps', 'apps'),
+                      View('Dev Log', 'devlog'),
+                      View('About', 'about'),
+                      View('Contact', 'contact')
+                )
+nav.init_app(app)
 
 @app.route("/")
 def index():
 	return render_template('index.html')
 
+@app.route("/home")
+def home():
+        return render_template('home.html')
+
+@app.route("/apps")
+def apps():
+        return render_template('home.html')
+
+@app.route("/devlog")
+def devlog():
+        return render_template('home.html')
+
+@app.route("/about")
+def about():
+        return render_template('home.html')
+
+@app.route("/contact")
+def contact():
+        return render_template('home.html')
+
 @app.route("/version")
 def version():
-	return "0.1.0"
+	return "0.1.2"
 
 @app.route("/static/<fname>")
 def get_resource(fname):
-	return send_file("/static/" + fname, mimetype='image/jpeg')
+	return send_file("static/" + fname, mimetype='image/jpeg')
 
 if __name__ == "__main__":
 	app.run()
