7803e077c6626f0d90d46bfc37aa766646d1cd9d
Toben Archer <sandslash@gmail.com>
2017-07-05 20:55:50 +0000

    Works now!
    I got the posts.py file working like I want it to now. Turns out
    there was a trailing '\n' causing all the problems. But I decided
    to upgrade the repos file to use json so I could add more info to
    the projects.
    
    In looking at this, I might release this as it's own project in the
    future. Autoblogging via git commit messages is pretty knifty and
    I think more people than just me would enjoy it. We'll see. Right
    now I'm jsut happy to get all the projects in to the system.

--- studiosleepygiraffe/resources/git-post/posts.py
+++ studiosleepygiraffe/resources/git-post/posts.py
@@ -1,4 +1,6 @@
 import git
+import json
+
 def getMessages(repo,branch):
 	ret = [com.message for com in repo.iter_commits('master')]
 	return ret
@@ -28,18 +30,44 @@ class Post:
 		ret = self.title
 		ret += "\n" + self.message
 		ret += "\n" + str(self.date)
-		ret += "\ntags: branch:" + self.branch + " author:" + self.author + " project:" + self.project + "\n\n"
+		ret += "\ntags: branch:" + str(self.branch) + " author:" + str(self.author) + " project:" + self.project + "\n\n"
 		return ret
 
-	
+	def json(self):
+		data = {}
+		data['author'] = str(self.author)
+		data['branch'] = str(self.branch)
+		data['project'] = str(self.project)
+		data['title'] = self.title
+		data['message'] = self.message
+		data['date'] = str(self.date)
+		return json.dumps(data)
+
+	def dict(self):
+		data = {}
+		data['author'] = str(self.author)
+		data['branch'] = str(self.branch)
+		data['project'] = str(self.project)
+		data['title'] = self.title
+		data['message'] = self.message
+		data['date'] = str(self.date)
+		return data
 
 
 if __name__ == "__main__":
-	repos = open("repos",'r').readlines()
+	repos = json.load(open("repos",'r'))
+	projects = []
 	for repo in repos:
+		project = repo
+		project['posts'] = []
+#		try:
 		branch = "master"
-		r = git.Repo("~/Eurus/ssg\ projects/" + repo + "/")
-		posts = getPosts(r,branch,repo)
+		r = git.Repo(repo['path'])
+		posts = getPosts(r,branch,repo['name'])
 		for p in posts:
-			print(p)
-
+			#print(p.json())
+			project['posts'].append(p.dict())
+#		except Exception as e:
+#			print("Skipping '{0}' because: {1}".format(repo,e))
+		projects.append(project)
+	json.dump(projects,open("projects.json",'w'))
