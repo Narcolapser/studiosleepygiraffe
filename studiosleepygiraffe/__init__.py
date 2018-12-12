9184431779023ba7ee45cd02a99e25a8620cb00d
Toben Archer <sandslash@gmail.com>
2018-12-12 16:45:07 -0600

    Updates to blog linking.
    There was a problem that was outstanding for a while that I've been
    wanting to fix but have never gotten around to it. blog posts have
    tags that when you click on them link you to a tags page so you can
    see all other posts with those tags. That last piece of it wasn't
    working. It would take you to a blank page. Got that sorted. I also
    moved the blog subsystem to its own blue print. This drops my
    __init__.py file down to about 100 lines of code. Probably about
    half the size it was when I started this endevour. Jolly good.

--- studiosleepygiraffe/__init__.py
+++ studiosleepygiraffe/__init__.py
@@ -3,13 +3,13 @@ from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
 from flask import Markup
-from xhtml2pdf import pisa
-from StringIO import StringIO
+
 import json
 import os
 import markdown
 
 from blueprints.snow import snow_api
+from blueprints.blog import blog_api
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__)) + "/"
@@ -18,6 +18,7 @@ BLOG_DIR = "/home/toben/Code/blog/"
 Bootstrap(app)
 
 app.register_blueprint(snow_api, url_prefix='/snow')
+app.register_blueprint(blog_api, url_prefix='/blog')
 
 def getAppFromRepos(app_name):
 	apps = json.load(open(APP_ROOT + "resources/repos.json"))
@@ -32,7 +33,7 @@ def mynavbar():
 	return Navbar("",
 			View('Home', 'home'),
 			View('Projects', 'apps'),
-			View('Blog', 'blog'), #not sure why but this expects me to put contact here...
+			View('Blog', 'blog'),
 			View('Dev Log', 'devlogs'),
 			View('About', 'about')
 		)
@@ -40,24 +41,11 @@ nav.init_app(app)
 
 @app.route("/")
 def index():
-#	return render_template('index.html')
-#	return render_template('houses.html')
 	return render_template('home.html')
-
-@app.route("/houses")
-def houses():
-	houses = os.listdir(APP_ROOT + "static/housepics/")
-	return render_template('houses.html',houses=houses)
-
-@app.route("/house/<house_name>")
-def house(house_name):
-	photolist = os.listdir(APP_ROOT + "static/housepics/"+house_name)
-	return render_template('house.html',photos=photolist,house=house_name)
 
 @app.route("/home")
 def home():
 	return render_template('home.html')
-#	return render_template('houses.html')
 
 @app.route("/apps")
 def apps():
@@ -93,51 +81,11 @@ def about():
 
 @app.route("/blog")
 def blog():
-	filenames = os.listdir(BLOG_DIR)
-	processed_posts = []
-	for i in filenames:
-		if '.json' in i:
-			post = getPostInfo(i)
-			if post:
-				processed_posts.append(post)
-	posts = {i['date']+": "+i['title']:i for i in processed_posts}
-	titles = posts.keys()
-	titles.sort(reverse=True)
-	return render_template('blog.html',posts=posts,titles=titles)
-
-def getPostInfo(fname):
-	try:
-		with open(BLOG_DIR + fname) as f:
-			content = json.load(f)
-		content['file'] = fname.replace('.json','')
-	except Exception as e:
-		print(e)
-		return None
-	return content
-
-@app.route("/blog/<post>")
-def blog_post(post):
-	with open(BLOG_DIR + post + '.json') as f:
-		content = json.load(f)
-	if content['content_file']:
-		md = open(BLOG_DIR + content['content_file']).read()
-		content['content'] = Markup(markdown.markdown(md))
-	return render_template('blogpost.html',title=post,content=content)
-
-@app.route("/blog/cover/<fname>")
-def get_cover(fname):
-	print("Getting cover:" + BLOG_DIR + "covers/" + fname)
-	if ".jpg" in fname:
-		return send_file(BLOG_DIR + "covers/" + fname, mimetype='image/jpeg')
-
-@app.route("/blog/tags/<tag>")
-def get_posts_with_tag(tag):
-	print("fetching posts with tag: " + tag)
-	return render_template('blogtag.html', tag=tag)
+	pass
 
 @app.route("/version")
 def version():
-	return "0.2.1"
+	return "0.3.0"
 
 @app.route("/static/<fname>")
 def get_resource(fname):
@@ -149,5 +97,7 @@ def get_resource(fname):
 		return send_file("static/"+fname,mimetype="text/script")
 	return send_file("static/" + fname, mimetype='image/jpeg')
 
+
 if __name__ == "__main__":
 	app.run()
+	
