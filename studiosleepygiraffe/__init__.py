a011c58b71e2da0f6610cf29fcd0a86b8cd7da4f
Toben Archer <sandslash@gmail.com>
2019-08-31 18:42:04 +0100

    Added several items.
    Before I can really call this update done I need to update all the
    data driven backend of it. So I've updated the markdown for
    automatic-fiesta, dashlib, kivy_joystick, and o365. I've also
    removed some of my older (and deader) projects.

--- studiosleepygiraffe/__init__.py
+++ studiosleepygiraffe/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, send_file
-from flask_bootstrap import Bootstrap
-from flask_nav import Nav
-from flask_nav.elements import Navbar, View
+#from flask_bootstrap import Bootstrap
+#from flask_nav import Nav
+#from flask_nav.elements import Navbar, View
 from flask import Markup
 from flask import request
 
@@ -9,22 +9,22 @@ import json
 import os
 import markdown
 
-from blueprints.snow import snow_api
-from blueprints.blog import blog_api
-from blueprints.resume import resume_api
+#from blueprints.snow import snow_api
+#from blueprints.blog import blog_api
+#from blueprints.resume import resume_api
 import iot
 
-from utils import visit
+#from utils import visit
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__)) + "/"
 BLOG_DIR = "/home/toben/Code/blog/"
 # BLOG_DIR = APP_ROOT + "static/blog/"
-Bootstrap(app)
+#Bootstrap(app)
 
 #app.register_blueprint(snow_api, url_prefix='/snow')
 #app.register_blueprint(blog_api, url_prefix='/blog')
-app.register_blueprint(resume_api, url_prefix='/resume')
+#app.register_blueprint(resume_api, url_prefix='/resume')
 
 
 def get_application_from_repositories(app_name):
@@ -35,26 +35,26 @@ def get_application_from_repositories(app_name):
     None
 
 
-nav = Nav()
+#nav = Nav()
 
 
-@nav.navigation()
-def mynavbar():
-    return Navbar("",
-            View('Home', 'home'),
-            View('Projects', 'apps'),
-            View('Blog', 'blog'),
-            View('Dev Log', 'devlogs'),
-            View('About', 'about')
-        )
+#@nav.navigation()
+#def mynavbar():
+#    return Navbar("",
+#            View('Home', 'home'),
+#            View('Projects', 'apps'),
+#            View('Blog', 'blog'),
+#            View('Dev Log', 'devlogs'),
+#            View('About', 'about')
+#        )
 
 
-nav.init_app(app)
+#nav.init_app(app)
 
 
-@app.before_request
-def init_log_visit():
-    visit.log_visit(request)
+#@app.before_request
+#def init_log_visit():
+#    visit.log_visit(request)
 
 
 @app.route("/")
