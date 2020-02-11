d8c4fd16be7f7af263cda8edca021020e171225b
Toben Archer <sandslash@gmail.com>
2020-02-10 20:33:10 -0600

    Moved images
    I moved the images into a sub folder of static to make that folder
    a little less crowded. I will be putting a number of .js files in
    there. I have now a basic app working with the react router. Now I
    just need to expand it to get all my old functionality back.

--- studiosleepygiraffe/__init__.py
+++ studiosleepygiraffe/__init__.py
@@ -21,115 +21,17 @@ def get_application_from_repositories(app_name):
         if application['url'] == app_name:
             return application
     None
-
+    
 
 #@app.before_request
 #def init_log_visit():
 #    visit.log_visit(request)
 
 
-@app.route("/")
-def index():
-    return render_template('home.html')
-
-
-@app.route("/home")
-def home():
-    return render_template('home.html')
-
-
-@app.route("/apps")
-def apps():
-    apps = json.load(open(APP_ROOT + "resources/repos.json"))
-    return render_template('apps.html',apps=apps)
-
-
-@app.route("/apps/<app_name>")
-def disp_app(app_name):
-    app = get_application_from_repositories(app_name)
-    print(app)
-    content = open(APP_ROOT + "resources/" + app['url'] + ".md").read()
-    content = Markup(markdown.markdown(content))
-    return render_template('app.html',app=app,content=content)
-
-
-@app.route("/devlogs")
-def devlogs():
-    apps = json.load(open(APP_ROOT + "resources/repos.json"))
-    return render_template('devlogs.html',apps=apps)
-    
-@app.route("/projects.json")
-def devlogsjson():
-    return open(APP_ROOT + "resources/repos.json").read()
-
-@app.route("/project/<name>")
-def project(name):
-    md = open(APP_ROOT + "resources/" + name + ".md").read()
-    content = Markup(markdown.markdown(md))
-    return content 
-
-@app.route("/devlog/<app_name>.json")
-def devlogjson(app_name):
-    content = {'name':app_name}
-    apps = json.load(open(APP_ROOT + "resources/projects.json"))
-    for post in apps[app_name]['posts']:
-        post['message'] = post['message'].replace('\n\n','<br>')
-        post['message'] = post['message'].replace('\n','')
-        post['message'] = post['message'].replace('<br>','\n\n')
-        post['date'] = post['date'][0:10]
-    return json.dumps(apps[app_name]['posts'])
-
-
-@app.route("/devlogs/<app_name>")
-def disp_logs(app_name):
-    content = {'name':app_name}
-    apps = json.load(open(APP_ROOT + "resources/projects.json"))
-    for post in apps[app_name]['posts']:
-        post['message'] = post['message'].replace('\n\n','<br>')
-        post['message'] = post['message'].replace('\n',' ')
-        post['message'] = post['message'].replace('<br>','\n\n')
-    return render_template('devlog.html',content=content,posts=apps[app_name]['posts'])
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-
-@app.route("/blog.json")
-def blog():
-	filenames = os.listdir(BLOG_DIR)
-	processed_posts = []
-	for i in filenames:
-		if '.json' in i:
-			post = getPostInfo(i)
-			if post:
-				processed_posts.append(post)
-	return json.dumps(processed_posts)
-#	posts = {i['date']+": "+i['title']:i for i in processed_posts}
-#	titles = list(posts.keys())
-#	titles.sort(reverse=True)
-#	return json.dumps(posts)
-
-@app.route("/blog/<mdfile>")
-def blogpost(mdfile):
-	md = open(BLOG_DIR + mdfile).read()
-	content = Markup(markdown.markdown(md))
-	return content
-
-def getPostInfo(fname):
-	try:
-		with open(BLOG_DIR + fname) as f:
-			content = json.load(f)
-		content['file'] = fname.replace('.json','')
-	except Exception as e:
-		print(e)
-		return None
-	return content
-
-@app.route("/version")
-def version():
-    return "0.3.0"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
 
 
 @app.route("/static/<file_name>")
@@ -140,11 +42,8 @@ def get_resource(file_name):
     print(file_name)
     if ".js" in file_name:
         return send_file("static/"+file_name, mimetype="text/script")
-    return send_file("static/" + file_name, mimetype='image/jpeg')
 
-@app.route("/components/<file_name>")
-def get_components(file_name):
-    return send_file("components/"+file_name, mimetype="text/script")
+    return send_file("static/" + file_name, mimetype='image/jpeg')
 
 
 @app.route("/iot/<device>/<method>")
