0d0aa98a546b600b8ed2a23993d23f246f202c2d
Toben Archer <sandslash@gmail.com>
2020-08-18 20:12:11 -0500

    Python API up to snuff with Ruby.
    Took me about 1.5 hours to get here with python. Took about 5.5 hours
    with ruby. So I'm about 4 times better with python than I am Ruby. Not
    bad.

--- python_api/api.py
+++ python_api/api.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify
 import sys
 import os
 import json
+import glob
 
 app = Flask(__name__)
 
@@ -34,6 +35,46 @@ def project_info(project):
 		return jsonify({'status':'failure'})
 	
 	return jsonify(json.loads(open('/home/toben/Code/ssg/'+project+'/info.json').read()))
+
+@app.route('/posts')
+def posts():
+	directories = glob.glob('/home/toben/Code/blog/*-*-*')
+	posts = []
+	for directory in directories:
+		post = {}
+		post['date'] = directory[22:]
+		post['url'] = '/posts/' + directory[22:]
+		info = json.load(open('{}/info.json'.format(directory)))
+		post['title'] = info['title']
+		post['cover'] = info['cover']
+		post['author'] = info['author']
+		
+		posts.append(post)
+	return jsonify(posts)
+
+@app.route('/posts/<post>')
+def post(post):
+	directories = glob.glob('/home/toben/Code/blog/*-*-*')
+	posts = [directory[22:] for directory in directories]
+	info = {'Status':'Failure'}
+	if post in posts:
+		info = json.load(open('/home/toben/Code/blog/{}/info.json'.format(post)))
+	return jsonify(info)
+
+@app.route('/logs')
+def logs():
+	projects = [json.loads(open(project).read()) for project in project_files]
+	projects.sort(key=_get_rank)
+	return jsonify(projects)
+
+@app.route('/logs/<project>')
+def log(project):
+	print(project)
+	print(project_names)
+	if project not in project_names:
+		return jsonify({'status':'failure'})
+	
+	return jsonify(json.loads(open('/home/toben/Code/ssg/'+project+'/logs.json').read()))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=3000)
